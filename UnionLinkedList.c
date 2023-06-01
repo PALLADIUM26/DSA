65f9d4c2311e11912ca4089293e6dff0d146a442
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-01 23:53:43 +0530

    Create UnionLinkedList.c

---
+++ UnionLinkedList.c
@@ -0,0 +1,28 @@
+//program to form union of 2 Linked Lists
+// List 1 = {1, 2 ,3 4}
+// List 2 = {2, 3, 4, 5}
+//resultant List = {1, 2, 3, 4, 5}
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+  int data;
+  struct Node* next;
+}Node;
+
+void append(){
+  
+}
+
+int present(){
+  
+}
+
+void union(){
+  
+}
+
+int main(){
+  return 0;
+}
