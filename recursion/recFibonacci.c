c286d2d64def5c78339b181f73a8e5333f511847
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-11 03:32:52 +0530

    Update recFibonacci.c

--- recursion/recFibonacci.c
+++ recursion/recFibonacci.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int fibo(int n){
+    if(n==0 || n==1)
+        return n;
+    return fibo(n-1)+fibo(n-2);
+}
+
+int main(){
+    int n, res;
+    printf("Enter the term: ");
+    scanf("%d",&n);
+    res = fibo(n);
+    printf("Fibonacci no. = %d\n",res);
+}
