f286d075eb2c235fea0132b761afbdf0a093ccf9
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-13 00:49:40 +0530

    Update tailRecFactorial.c

--- recursion/tailRecFactorial.c
+++ recursion/tailRecFactorial.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int fact(int n, int a){
+    if(n == 0)
+        return a;
+    return (fact(n-1, a*n));
+}
+
+int main(){
+    int num;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    int res = fact(num, 1);
+    printf("%d\n",res);
+    return 0;
+}
