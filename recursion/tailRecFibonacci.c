f55afcb5f3c028cdd192455a7b8de812a9db6c7f
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-14 00:21:53 +0530

    Update tailRecFibonacci.c

--- recursion/tailRecFibonacci.c
+++ recursion/tailRecFibonacci.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int fibo(int n, int a, int b){
+    if(n == 0)
+        return a;
+    return fibo(n-1, a+b, a);
+}
+
+int main(){
+    int num;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    int res = fibo(num, 0, 1);
+    printf("%d\n",res);
+    return 0;
+}
