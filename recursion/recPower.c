c624342ed85813f55881a753cd1e3f9b1257c02a
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-22 00:51:34 +0530

    Update recPower.c

--- recursion/recPower.c
+++ recursion/recPower.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int power(int a, int b){
+    if(b==1)
+        return a;
+    return a*power(a,b-1);
+}
+
+int main(){
+    int a, b, res;
+    scanf("%d %d",&a, &b);
+    res = power(a,b);
+    printf("Answer = %d",res);
+    return 0;
+}
