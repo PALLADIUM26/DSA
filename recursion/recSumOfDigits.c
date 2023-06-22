fccd8215f7b6916aada89b5edcbc093965fda19a
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-06-23 02:15:16 +0530

    Update recSumOfDigits.c

--- recursion/recSumOfDigits.c
+++ recursion/recSumOfDigits.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int func(int n){
+    if(n==0)
+        return 0;
+    return (n%10)+func(n/10);
+}
+
+int main(){
+    int num, res;
+    printf("Enter number: ");
+    scanf("%d",&num);
+    res = func(num);
+    printf("Sum of Digits of %d = %d\n",num,res);
+    return 0;
+}
