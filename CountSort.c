d9694a979a4fbac8de3ff80ba8490c2d14f67cf2
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-07-05 04:10:17 +0530

    Update CountSort.c

--- CountSort.c
+++ CountSort.c
@@ -1,2 +1,28 @@
 #include <stdio.h>
 
+void countSort(int a[], int n){
+    int max = 0, p = 0, i;
+    for(i=0; i<n; i++){
+        if(a[i] > max)
+            max = a[i];
+    }
+
+    int count[max+1];
+
+    for(i=0; i<=max; i++)
+        count[i] = 0;
+    
+    for(i=0; i<n; i++)
+        count[a[i]]++;
+    
+    i = 0;
+    while(i <= max){
+        if(count[i] > 0){
+            a[p] = i;
+            p++;
+            count[i]--;
+        }
+        else
+            i++;
+    }
+}
