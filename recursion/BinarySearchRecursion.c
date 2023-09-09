4085825036f53a776c264f4c8581de794e0e7b91
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-09-09 09:37:07 +0530

    Update BinarySearchRecursion.c

--- recursion/BinarySearchRecursion.c
+++ recursion/BinarySearchRecursion.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+
+int binSer(int a[], int l, int u, int search){
+    if(l <= u){
+        int m = (l + (u-1))/2;
+        if(a[m] == search)
+            return m;
+        else if(a[m] < search)
+            return binSer(a, m+1, u, search);
+        else
+            return binSer(a, l, m-1, search);
+    }
+}
+
+int main(){
+    int arr[5] = {5, 6, 10, 12, 19};
+    int x = binSer(arr, 0, 4, 10);
+    printf("%d", x);
+    return 0;
+}
 
