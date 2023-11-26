be88c7a536abc3fc1553392066a37727a53b0804
Pranith Dutta <116259268+PALLADIUM26@users.noreply.github.com>
2023-11-26 10:47:18 +0530

    Update LCS.c

--- Algorithms/LCS.c
+++ Algorithms/LCS.c
@@ -1,1 +1,50 @@
+#include<stdio.h>
+#include<string.h>
 
+int max(int a, int b){
+    return (a > b) ? a : b;}
+
+int lcs(char *a,char *b){
+    int m = strlen(a);
+    int n = strlen(b);
+    int x = (m>n)?m:n;
+    int y = (m<n)?m:n;
+    m = y;
+    n = x;
+
+    int lcs[m+1][n+1];
+
+    for(int i=0;i<=m;i++)
+    lcs[i][0]=0;
+    
+    for(int j=0;j<=n;j++)
+    lcs[0][j]=0;
+    
+    for(int i=1;i<=m;i++) {
+        for(int j=1;j<=n;j++) {
+            if(a[i-1]==b[j-1])
+            lcs[i][j]=1+lcs[i-1][j-1];
+            else
+            lcs[i][j]=max(lcs[i-1][j],lcs[i][j-1]);
+        }
+    }
+    for (int i = 0; i <= m; i++) {
+        for (int j = 0; j <= n; j++)
+            printf("%d ", lcs[i][j]);
+        printf("\n");
+    }
+    return lcs[m][n];
+}
+
+int main()
+{
+    int num1,num2;
+    char a[100];
+    char b[100];
+    printf("Enter Your String 1: ");
+    scanf("%s", a);
+    printf("\nEnter Your 2nd String 2: ");
+    scanf("%s", b);
+    int length=lcs(a,b);
+    printf("\nLength of Longest Common Subsequence: %d\n", length);
+}
